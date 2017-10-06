5f70f6eeabf9fbb23b74b712e7bf5d66250efaf3
marinablack1992 <marinablack1992@gmail.com>
2017-10-06 11:52:00 -0600

    front end

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -10,29 +10,18 @@ const GET_USER_INFO = 'GET_USER_INFO';
 const UPDATE_USER = 'UPDATE_USER';
 const REMOVE_FRIEND = 'REMOVE_FRIEND';
 
-export function addFriend(){
-    return {
-        type: ADD_FRIEND,
-    }
-}
+// export function addFriend(){
+//     return {
+//         type: ADD_FRIEND,
+//     }
+// }
 
-export function getFriend(){
-    return {
-        type: GET_FRIEND,
-    }
-}
+// export function getFriend(){
+//     return {
+//         type: GET_FRIEND,
+//     }
+// }
 
-export function getUserInfo(){
-    return {
-        type: GET_USER_INFO,
-    }
-}
-
-export function getFriend(){
-    return {
-        type: GET_FRIEND,
-    }
-}
 
 export default function reducer(state = initialState, action){
 
